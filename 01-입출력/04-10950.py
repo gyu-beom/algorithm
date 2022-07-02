68bc3e250ef0f25ca768fa41e476792cded62a2a
gyu-beom <computer.goosebumps96@gmail.com>
2022-07-03 06:09:45 +0900

    220703 - 01-입출력 - 01-10

---
+++ 01-입출력/04-10950.py
@@ -0,0 +1,4 @@
+test_case = int(input())
+for _ in range(test_case):
+    x, y = map(int, input().split())
+    print(x + y)
---
+++ 01-입출력/09-11022.py
@@ -0,0 +1,4 @@
+test_case = int(input())
+for idx in range(test_case):
+    x, y = map(int, input().split())
+    print(f'Case #{idx + 1}: {x} + {y} = {x + y}')
